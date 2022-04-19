4d56aff33cbbd2c10f8d7f2b743eb47ce4960588
rninji <yellowminji@naver.com>
2022-04-19 23:59:13 +0900

    220419 add js more

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var submit =document.querySelector('#submit');
 var rating = document.querySelector('#rating');
 var thank = document.querySelector('#thank-you');
+var state = document.querySelectorAll('.state');
+
+var selectState = 1;
 
 function clickSubmit(){
     rating.classList.add('hidden');
     thank.classList.remove('hidden');
+    document.getElementById("select-rating").innerHTML=selectState;
+}
+
+function clickState(){
+    this.classList.add('active');
+    for (const button of state){
+        if(button!=this) button.classList.remove('active');
+    }
+    selectState = this.innerText;
 }
 
 submit.addEventListener('click', clickSubmit);
+for (const button of state){
+    button.addEventListener('click', clickState);
+}
+
